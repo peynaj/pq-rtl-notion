4031f28602a19174ea361d33371262fd5b83eef7
peynaj <peynajber@gmail.com>
2021-09-08 18:55:57 +0430

    Refactor scroll buttons

--- background.js
+++ background.js
@@ -1,29 +1,33 @@
 console.log("===> Hello Notion from background.js of pq-rtl-notion.")
 
-const horizontalScroller = document.getElementsByClassName("notion-scroller vertical horizontal");
-console.log("pq-rtl-notion", {horizontalScroller, length: horizontalScroller.length});
-for (let i = 0; i < horizontalScroller.length; i++) {
-    let div = horizontalScroller[i];
-    div.scrollLeft = 1_000_000;  // not work before page full load
-
-    let scrollElem1 = document.createElement("button");
-    scrollElem1.innerText = ">"
-    scrollElem1.classList.add("scroll-to-right")
-    scrollElem1.title = ">> Scroll To Right >>"
-    scrollElem1.onclick = () => {
-        div.scrollLeft = 1_000_000;
-    }
-    div.appendChild(scrollElem1)
-
-    let scrollElem2 = document.createElement("button");
-    scrollElem2.innerText = "^"
-    scrollElem2.classList.add("scroll-to-top")
-    scrollElem2.title = "^^ Scroll To Top ^^"
-    scrollElem2.onclick = () => {
-        div.scrollTop = 0;
-    }
-    div.appendChild(scrollElem2)
+let horizontalScroller = document.getElementsByClassName("notion-scroller vertical horizontal");
+// console.log("pq-rtl-notion", {horizontalScroller, length: horizontalScroller.length});
+if (horizontalScroller.length > 0) {
+    horizontalScroller[0].scrollLeft = 1_000_000;  // not work before page full load
 }
+
+let scrollElem1 = document.createElement("button");
+scrollElem1.innerText = ">"
+scrollElem1.classList.add("scroll-to-right")
+scrollElem1.title = ">> Scroll To Right >>"
+scrollElem1.onclick = () => {
+    let horizontalScroller = document.getElementsByClassName("notion-scroller vertical horizontal");
+    if (horizontalScroller.length > 0)
+        horizontalScroller[0].scrollLeft = 1_000_000;
+}
+document.body.appendChild(scrollElem1)
+
+let scrollElem2 = document.createElement("button");
+scrollElem2.innerText = "^"
+scrollElem2.classList.add("scroll-to-top")
+scrollElem2.title = "^^ Scroll To Top ^^"
+scrollElem2.onclick = () => {
+    let horizontalScroller = document.getElementsByClassName("notion-scroller vertical horizontal");
+    if (horizontalScroller.length > 0)
+        horizontalScroller[0].scrollTop = 0;
+}
+document.body.appendChild(scrollElem2)
+
 
 
 let extensionNameElem = document.createElement("a");
